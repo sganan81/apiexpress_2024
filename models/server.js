5058da0bd979ce0a76de847499c38ed62394d0af
Sebastian Gañan <sganan81@gmail.com>
2024-08-24 12:02:19 -0300

    Commit inicial

---
+++ models/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+class Server {
+
+    constructor(){        
+        this.app = express();
+        this.port = process.env.PORT || 3000;   
+        this.rutas();
+    }
+
+    rutas(){
+        this.app.use('/api/v1/peliculas', require('../routes/peliculas')); // Integrante 1
+        this.app.use('/api/v1/actores', require('../routes/peliculas')); // Integrante 2
+    }
+
+    listen(){
+        this.app.listen(this.port, ()=>{
+            console.log(`La API esta escuchando en el this.PORT ${this.port}`)
+        });
+    }
+}
+
+module.exports = Server;
