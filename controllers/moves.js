a281726a5d812cfe3548737af22dac3fd4231303
GastonIvanHaagGomez <gasel88@hotmail.com>
2024-09-21 20:56:58 -0300

    agregue validaciones

--- controllers/moves.js
+++ controllers/moves.js
@@ -3,8 +3,9 @@ const { request, response } = require("express");
 const { apiUrl } = require('../config/config')
 
 const getMoves = (req = request, res = response) => {
-  const { limit = 50 } = req.query;
-  const filtro = `?limit=${limit}`;
+  const { limit = 50, page = 1 } = req.query
+  const offset = (page - 1) * limit
+  const filtro = `?limit=${limit}&offset=${offset}`
 
   axios
   .get(apiUrl+`/move`+`${filtro}`)
@@ -33,6 +34,19 @@ const getMoves = (req = request, res = response) => {
 
 const getMove = (req = request, res = response) => {
   const { idMove } = req.params;
+
+  if (!idMove) {
+    return res.status(400).json({
+      msg: 'El parámetro idMove es requerido',
+    });
+  }
+
+  const idRegex = /^[0-9]+$/; 
+  if (!idRegex.test(idMove)) {
+    return res.status(422).json({
+      msg: 'El parámetro idMove debe contener solo números',
+    });
+  }
 
   axios
     .get(apiUrl+`/move/${idMove}`)
