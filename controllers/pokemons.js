cb1491cb9832ca72d4ef89be6e82751d69894e45
Reiken27 <emiliano.correa.amigo@gmail.com>
2024-09-21 18:49:07 -0300

    cambios a la api, y agrego configuracion para el .env

--- controllers/pokemons.js
+++ controllers/pokemons.js
@@ -1,51 +1,75 @@
-const axios = require("axios");
-const { request, response } = require("express");
+const axios = require('axios')
+const { request, response } = require('express')
+const { apiUrl } = require('../config/config')
+
 
 const getPokemons = (req = request, res = response) => {
-  const { results, count, next, previous } = req.query;
-  console.log(results, count, next, previous);
-  const filtro = results ? `?id=${results}` : "";
+  const { limit = 50, page = 1 } = req.query
+  const offset = (page - 1) * limit
+  const filtro = `?limit=${limit}&offset=${offset}`
+
   axios
-    .get(`https://pokeapi.co/api/v2/pokemon/${filtro}`)
+    .get(apiUrl+`/pokemon`+`${filtro}`)
     .then((response) => {
-      const { data } = response;
+      const { data } = response
 
       res.status(200).json({
-        msg: "Ok",
-        data,
-      });
+        msg: 'Ok',
+        data
+      })
     })
     .catch((error) => {
-      console.log(error);
-      res.status(400).json({
-        msg: "Error",
-        error,
-      });
-    });
-};
+      if (error.response && error.response.status === 404) {
+        return res.status(404).json({
+          msg: 'No se encontró el Pokémon con el id proporcionado'
+        })
+      }
+      res.status(500).json({
+        msg: 'Error en el servidor',
+        error
+      })
+    })
+}
 
 const getPokemon = (req = request, res = response) => {
-  const { idPokemon = "" } = req.params;
-  console.log(idPokemon);
+  const { nombre_pokemon = '' } = req.params
+
+  if (!nombre_pokemon) {
+    return res.status(400).json({
+      msg: 'El parámetro nombre_pokemon es requerido'
+    })
+  }
+
+  const nameRegex = /^[A-Za-z]+$/
+  if (!nameRegex.test(nombre_pokemon)) {
+    return res.status(422).json({
+      msg: 'El parámetro nombre_pokemon debe contener solo letras (no se permiten números ni caracteres especiales)'
+    })
+  }
+
   axios
-    .get(`https://pokeapi.co/api/v2/${idPokemon}`)
+    .get(apiUrl+`/pokemon`+`${nombre_pokemon}`)
     .then((response) => {
-      const { data } = response;
+      const { data } = response
       res.status(200).json({
-        msg: "Ok",
-        data,
-      });
+        msg: 'Ok',
+        data
+      })
     })
     .catch((error) => {
-      console.log(error);
-      res.status(400).json({
-        msg: "Error",
-        error,
-      });
-    });
-};
+      if (error.response && error.response.status === 404) {
+        return res.status(404).json({
+          msg: 'No se encontró el Pokémon con el nombre proporcionado'
+        })
+      }
+      res.status(500).json({
+        msg: 'Error en el servidor',
+        error
+      })
+    })
+}
 
 module.exports = {
   getPokemons,
-  getPokemon,
-};
+  getPokemon
+}
--- readme.md
+++ readme.md
@@ -1,5 +1,8 @@
 ## API con express creada con NodeJS
 
+La api no requiere clave, se puede ejecutar con
+node app.js
+
 Endpoint "Obtener un Pokémon por ID" - Evelin Paumgertner:
 1 - Registro de ID Pokemons:
 http://localhost:3000/id_pokemon
@@ -7,3 +10,11 @@ http://localhost:3000/id_pokemon
 http://localhost:3000/id_pokemon/1
 3 - Query params:
 http://localhost:3000/id_pokemon?limit=10&page=2
+
+Endpoint "Obtener un Pokémon por Nombre de pokemon" - Emiliano Correa:
+1 - Registro de nombres:
+http://localhost:3000/pokemon
+2 - Buscar pokemon por nombre de pokemon:
+http://localhost:3000/pokemon/1
+3 - Query params:
+http://localhost:3000/pokemon?limit=10&page=2
