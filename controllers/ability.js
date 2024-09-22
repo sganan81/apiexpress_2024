f74b2c5bef8303e1ce4075ded0191b3d96909485
Facundo Reiseng <facu_carhue@hotmail.com>
2024-09-21 21:27:53 -0300

    Cambios en status codes y readme

--- controllers/ability.js
+++ controllers/ability.js
@@ -17,31 +17,39 @@ const getAbilities = (req = request, res = response) => {
       });
     })
     .catch((error) => {
-      console.log(error);
-      res.status(400).json({
-        msg: "Error",
-        error,
-      });
-    });
+      if (error.response && error.response.status === 404) {
+        return res.status(404).json({
+          msg: 'Error al obtener las habilidades',
+        })
+      }
+      res.status(500).json({
+        msg: 'Error en el servidor',
+        error
+      })
+    })
 };
 const getAbility = (req = request, res = response) => {
   const { idAbility = "" } = req.params; 
   axios
     .get(`${apiUrl}/ability/${idAbility}`) 
     .then((response) => {
-      const { data } = response;
+      const { data } = response
       res.status(200).json({
-        msg: "Ok",
-        data,
-      });
+        msg: 'Ok',
+        data
+      })
     })
     .catch((error) => {
-      console.log(error);
-      res.status(400).json({
-        msg: "Error",
-        error,
-      });
-    });
+      if (error.response && error.response.status === 404) {
+        return res.status(404).json({
+          msg: 'No se encontró la habilidad con el nombre o id proporcionado'
+        })
+      }
+      res.status(500).json({
+        msg: 'Error en el servidor',
+        error
+      })
+    })
 };
 
 module.exports = {
