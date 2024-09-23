ced1ab9d748f787d4bb5b5f36d011231c9a5eccf
Lucasbertone16 <danielaabertone60@gmail.com>
2024-09-23 20:07:09 -0300

    feat: Endpoints cumpliendo consigna y actualizamos read.md

--- controllers/clima.js
+++ controllers/clima.js
@@ -1,50 +1,5 @@
 const axios = require('axios')
 
-const getClimaActualPorCiudadFiltrado = async (req, res) => {
-  const { ciudad, lang = 'sp' } = req.query
-
-  if (!ciudad) {
-    return res.status(400).json({ error: 'La ciudad es necesaria para continuar.' })
-  }
-
-  axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${process.env.API_KEY}&lang=${lang}&units=metric`)
-    .then((response) => {
-      const { main, wind, weather, name } = response.data
-
-      const climaFiltrado = {
-        ciudad: name,
-        descripcion: weather[0].description,
-        temperatura_maxima: main.temp_max,
-        temperatura_minima: main.temp_min,
-        sensacion_termica: main.feels_like,
-        viento: wind.speed
-      }
-
-      res.status(200).json({
-        msg: 'Ok',
-        clima: climaFiltrado
-      })
-    })
-    .catch((error) => {
-      console.error(error)
-      if (error.response) {
-        return res.status(error.response.status).json({
-          status: 'error',
-          msg: 'Error al obtener datos del clima',
-          error: error.response.data.message || error.response.statusText,
-          statusCode: error.response.status
-        })
-      } else {
-        res.status(500).json({
-          status: 'error',
-          msg: 'Error inesperado al obtener la información',
-          error: error.message,
-          statusCode: 500
-        })
-      }
-    })
-}
-
 const getClimaActualPorCiudad = async (req, res) => {
   const { ciudad, lang = 'sp' } = req.query
 
@@ -80,4 +35,4 @@ const getClimaActualPorCiudad = async (req, res) => {
     })
 }
 
-module.exports = { getClimaActualPorCiudadFiltrado, getClimaActualPorCiudad }
+module.exports = { getClimaActualPorCiudad }
--- routes/clima.js
+++ routes/clima.js
@@ -1,8 +1,8 @@
 const { Router } = require('express')
-const { getClimaActualPorCiudad, getClimaActualPorCiudadFiltrado } = require('../controllers/clima')
+const { getClimaActualPorCiudad } = require('../controllers/clima')
 const rutas = Router()
 
 rutas.get('/ciudad', getClimaActualPorCiudad)
-rutas.get('/ciudadFiltrado', getClimaActualPorCiudadFiltrado)
+
 
 module.exports = rutas
