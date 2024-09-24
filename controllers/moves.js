372c6148c269a905ce701a7a105aa624fa69c7d6
GastonIvanHaagGomez <Gasel88@hotmail.com>
2024-09-24 12:44:23 -0300

    otros cambios

--- controllers/moves.js
+++ controllers/moves.js
@@ -1,63 +1,55 @@
 const axios = require("axios");
 const { request, response } = require("express");
-const { apiUrl } = require('../config/config')
+const { apiUrl } = require('../config/config');
 
 const getMoves = (req = request, res = response) => {
-  const { limit = 50, page = 1 } = req.query
-  const offset = (page - 1) * limit
-  const filtro = `?limit=${limit}&offset=${offset}`
+  const { limit = 50, page = 1 } = req.query;
+  const offset = (page - 1) * limit;
+  const filtro = `?limit=${limit}&offset=${offset}`;
 
   axios
-  .get(apiUrl+`/move`+`${filtro}`)
+    .get(`${apiUrl}/move${filtro}`)
     .then((response) => {
       const { data } = response;
       res.status(200).json({
-        status: 'ok',
+        msg: "Ok",
         data,
       });
     })
     .catch((error) => {
-      console.error(error);
-      if (error.response) {
-        res.status(400).json({
-          status: 'error',
-          msg: 'Bad Request',
-        });
-      } else {
-        res.status(500).json({
-          status: 'error',
-          msg: 'Error inesperado al obtener la información',
+      if (error.response && error.response.status === 404) {
+        return res.status(404).json({
+          msg: 'Error al obtener los movimientos',
         });
       }
+      res.status(500).json({
+        msg: 'Error en el servidor',
+        error
+      });
     });
 };
 
 const getMove = (req = request, res = response) => {
-  const { idMove } = req.params;
-
-
+  const { idMove = "" } = req.params; 
   axios
-    .get(apiUrl+`/move/${idMove}`)
+    .get(`${apiUrl}/move/${idMove}`)
     .then((response) => {
       const { data } = response;
       res.status(200).json({
-        status: 'ok',
-        data,
+        msg: 'Ok',
+        data
       });
     })
     .catch((error) => {
-      console.error(error);
-      if (error.response) {
-        res.status(400).json({
-          status: 'error',
-          msg: 'Bad Request',
-        });
-      } else {
-        res.status(500).json({
-          status: 'error',
-          msg: 'Error inesperado al obtener la información',
+      if (error.response && error.response.status === 404) {
+        return res.status(404).json({
+          msg: 'No se encontró el movimiento con el nombre o id proporcionado'
         });
       }
+      res.status(500).json({
+        msg: 'Error en el servidor',
+        error
+      });
     });
 };
 
